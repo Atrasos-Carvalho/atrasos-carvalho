edf8b0d2c484b32e52478a822e5d405bbe6dda60
RafaPizza3 <rafapitherpizza@gmail.com>
2026-04-30 11:11:24 -0300

    adicionando create

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 function registrar(tipo) {
   if (tipo == "popHorario") {
-    popupH = document.getElementById("popHorario");
-    popupH.style.marginLeft = "0";
-    setTimeout(() => {
-      popupH.style.marginLeft = "-700px";
-    }, 3500);
+    create(1).then(() => {
+      popupH = document.getElementById("popHorario");
+      popupH.style.marginLeft = "0";
+      setTimeout(() => {
+        popupH.style.marginLeft = "-700px";
+      }, 3500);
+    });
   } else if (tipo == "popAtraso") {
-    popupA = document.getElementById("popAtraso");
-    popupA.style.marginLeft = "0";
-    setTimeout(() => {
-      popupA.style.marginLeft = "-700px";
-    }, 3500);
+    create(2).then(() => {
+      popupA = document.getElementById("popAtraso");
+      popupA.style.marginLeft = "0";
+      setTimeout(() => {
+        popupA.style.marginLeft = "-700px";
+      }, 3500);
+    });
   } else if (tipo == "popFalta") {
-    popupF = document.getElementById("popFalta");
-    popupF.style.marginLeft = "0";
-    setTimeout(() => {
-      popupF.style.marginLeft = "-700px";
-    }, 3500);
+    create(3).then(() => {
+      popupF = document.getElementById("popFalta");
+      popupF.style.marginLeft = "0";
+      setTimeout(() => {
+        popupF.style.marginLeft = "-700px";
+      }, 3500);
+    });
+  }
+}
+
+async function create(id) {
+  try {
+    const res = await fetch(
+      "https://ms-atrasos-carvalho.onrender.com/url/registros/create",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id: id }),
+      },
+    );
+  } catch (error) {
+    console.error("Erro: ", error);
   }
 }
