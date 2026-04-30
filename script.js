d589afee84e670c67d1185b6f3c042a1ef6fed40
RafaPizza3 <rafapitherpizza@gmail.com>
2026-04-30 18:00:59 -0300

    colocando o resto das coisas

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-function registrar(tipo) {
-  if (tipo == "popHorario") {
-    create(1).then(() => {
-      popupH = document.getElementById("popHorario");
-      popupH.style.marginLeft = "0";
+fazerCounts();
+
+function registrar(tipo, id) {
+  create(id).then(() => {
+    const popup = document.getElementById(tipo);
+
+    if (popup) {
+      popup.style.marginLeft = "0";
       setTimeout(() => {
-        popupH.style.marginLeft = "-700px";
+        popup.style.marginLeft = "-700px";
       }, 3500);
-    });
-  } else if (tipo == "popAtraso") {
-    create(2).then(() => {
-      popupA = document.getElementById("popAtraso");
-      popupA.style.marginLeft = "0";
-      setTimeout(() => {
-        popupA.style.marginLeft = "-700px";
-      }, 3500);
-    });
-  } else if (tipo == "popFalta") {
-    create(3).then(() => {
-      popupF = document.getElementById("popFalta");
-      popupF.style.marginLeft = "0";
-      setTimeout(() => {
-        popupF.style.marginLeft = "-700px";
-      }, 3500);
-    });
-  }
+    }
+  });
 }
 
 async function create(id) {
   try {
-    const res = await fetch(
+    const res = await axios.post(
       "https://ms-atrasos-carvalho.onrender.com/url/registros/create",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ id: id }),
-      },
+      { id: id },
     );
+  } catch (error) {
+    if (error.response && error.response.status === 409) {
+      const popupE = document.getElementById("popErro");
+      popupE.style.marginLeft = "0";
+      setTimeout(() => {
+        popupE.style.marginLeft = "-700px";
+      }, 3500);
+    }
+    console.error("Erro: ", error);
+  }
+}
+
+async function getCount() {
+  try {
+    const res = await axios.get(
+      "https://ms-atrasos-carvalho.onrender.com/url/registros/find-count",
+    );
+
+    return res.data;
   } catch (error) {
     console.error("Erro: ", error);
   }
+}
+
+function fazerCounts() {
+  const hor = document.getElementById("horario");
+  const atr = document.getElementById("atrasado");
+  const fal = document.getElementById("falta");
+
+  getCount().then(data => {
+    if (data) {
+      hor.textContent = data.total_cedo;
+      atr.textContent = data.total_atraso;
+      fal.textContent = data.total_falta;
+    }
+  });
 }
